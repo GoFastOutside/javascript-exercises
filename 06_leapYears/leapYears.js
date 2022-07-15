1bee84132bed6875732124da1014b411f78e0a84
Derek <dblank7@gmail.com>
2022-07-15 06:14:26 -0600

    Complete exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    console.log("year % 4:"+year % 4+" year%100==0:"+year%100==0+"  year%400 ")
+    if((year % 4==0) && !(year % 100==0)  ||  (year%100==0  &&  year%400==0)){
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
