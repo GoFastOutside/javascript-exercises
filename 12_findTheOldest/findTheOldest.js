6820abefbf226e64a344f9d53a94827d69184261
Derek <dblank7@gmail.com>
2022-08-02 00:01:50 -0600

    Complete exorcise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people=people.map(function(person){
+        if(!person.yearOfDeath){
+            person.yearOfDeath=(new Date).getFullYear()
+            }
+            return person;
+    });
+    people=people.sort((a,b)=>(a.yearOfDeath-a.yearOfBirth) > (b.yearOfDeath-b.yearOfBirth)? -1:1);
+    return people[0];
 
 };
 
