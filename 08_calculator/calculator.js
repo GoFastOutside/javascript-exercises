8853bdb7f55c171fb06d25573f4af2211722061e
Derek <dblank7@gmail.com>
2022-08-01 19:54:43 -0600

    comeplete exc 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
 const add = function() {
-	
+	let sum = Array.from(arguments).reduce((a,b)=>a+b,0);
+  return sum.typeOf='number'?sum:0;
 };
 
 const subtract = function() {
-	
+  return Array.from(arguments).reduce((a,b)=>a-b,arguments[0]*2);
+
 };
 
 const sum = function() {
-	
+  let sum = +Array.from(... arguments).reduce((a,b)=>a+b,0);
+  return sum.typeOf='number'?sum:0; 
 };
 
 const multiply = function() {
-
+  let product= +Array.from(... arguments).reduce((a,b)=>a*b,1);
+  return  product.typeOf='number'? product:0; 
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if(n===0){return 1;}
+  let total=1;
+  for (let i=1;i<=n;i++){
+    total=total*i;
+  }
+  return total;
 };
 
 // Do not edit below this line
