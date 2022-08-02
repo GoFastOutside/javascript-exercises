7bea8434221f917e6249536c1ec6ee1d93a0ed3d
Derek <dblank7@gmail.com>
2022-08-01 23:20:27 -0600

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+    let fib=[1 , 1];
 
+    if (position<1){return 'OOPS';}
+
+    for (let i=2;i<=position;i++){
+        fib.push(fib[i-1]+fib[i-2]);
+    }
+    return fib[position-1];
 };
 
 // Do not edit below this line
